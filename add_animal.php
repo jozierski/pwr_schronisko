dfcc93b01631e2b7dedc4a27ad3213937abf5ead
jozierski <31972544+jozierski@users.noreply.github.com>
2021-05-25 13:47:55 +0200

    Widok dodawania zwierząt

--- add_animal.php
+++ add_animal.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-require_once('dataBase.php');
 if (isset($_SESSION['zalogowany']) == false) {
   header("Location: error.php");
   die();
 }
 ?>
+
 <!doctype html>
 <html lang="en">
 
@@ -13,9 +13,7 @@ if (isset($_SESSION['zalogowany']) == false) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>Strona Główna | Wesoły Zwierzak</title>
-
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
+  <title>Nowy zwierzak | Wesoły Zwierzak</title>
 
   <!-- Bootstrap core CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
@@ -23,6 +21,7 @@ if (isset($_SESSION['zalogowany']) == false) {
   <!-- Custom styles for this template -->
   <link href="style.css" rel="stylesheet">
 </head>
+
 <body>
   <header>
     <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
@@ -86,52 +85,52 @@ if (isset($_SESSION['zalogowany']) == false) {
   <main role="main">
     <div class="container">
       <div class="container marketing">
+        <br>
         <div class="row featurette" style="text-align: center;">
-          <h2 class="featurette-heading"><strong>Zarządzaj zwierzętami</strong></h2>
+          <h2 class="featurette-heading"><strong>Utwórz nowe konto</strong></h2>
         </div>
+
         <hr class="featurette-divider">
-        <?php
-        $sqlQuery = "SELECT * FROM animals";
-        $resultSet = mysqli_query($conn, $sqlQuery) or die("database error:" . mysqli_error($conn));
-        ?>
-        <table id="editableTable" class="table table-bordered">
-          <thead>
-            <tr>
-              <th>Animal_id</th>
-              <th>Imie</th>
-              <th>Rodzaj</th>
-              <th>Nr. klatki</th>
-              <th>ID opiekuna</th>
-              <th>Płeć</th>
-              <th>Kastracja/Sterylizacja?</th>
-              <th>Nr. chipa</th>
-              <th>Wiek</th>
-              <th>Data dodania</th>
-              <th>Stan</th>
-              <th> </th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php while ($row = mysqli_fetch_assoc($resultSet)) { ?>
-              <tr id="<?php echo $row['animal_id']; ?>">
-                <td><?php echo $row['animal_id']; ?></td>
-                <td><?php echo $row['imie']; ?></td>
-                <td><?php echo $row['rodzaj']; ?></td>
-                <td><?php echo $row['nr_klatki']; ?></td>
-                <td><?php echo $row['opiekun']; ?></td>
-                <td><?php echo $row['plec']; ?></td>
-                <td><?php echo $row['wykastrowany']; ?></td>
-                <td><?php echo $row['nr_chipa']; ?></td>
-                <td><?php echo $row['szacowany_wiek']; ?></td>
-                <td><?php echo $row['data_dodania']; ?></td>
-                <td><?php echo $row['stan']; ?></td>
-                <td><a href="#" class="link-success">EDYTUJ</a></td>
-              </tr>
-            <?php } ?>
-          </tbody>
-        </table>
+
+        <!-- Login Form -->
+        <form style="width:30%; margin-left:35%;" action="./scryptAddAnimal.php" method="post">
+          Imie:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+          <input type="text" id="imie" name="imie" placeholder="imie" required><br><br>
+
+          Nr. Klatki:&nbsp&nbsp&nbsp&nbsp&nbsp
+          <input type="number" id="nr_klatki" name="nr_klatki" placeholder="nr. klatki" required><br><br>
+
+          ID Opiekuna:
+          <input type="number" id="opiekun" name="opiekun" placeholder="id opiekuna" required><br><br>
+          
+          <div style="text-align:center">
+          Nr. Chip'a (jeśli zachipowany):
+          <input type="number" id="nr_chipa" name="nr_chipa" placeholder="##########" pattern="[0-9]{10}" size="10 "><br><br>
+
+          Rodzaj<br>
+          <input type="radio" id="pies" name="rodzaj" value="pies" required>
+          <label for="pies">Pies</label><br>
+          <input type="radio" id="kot" name="rodzaj" value="kot" required>
+          <label for="kot">Kot</label><br><br>
+
+          Płeć<br>
+          <input type="radio" id="samiec" name="plec" value="samiec" required>
+          <label for="samiec">Samiec</label><br>
+          <input type="radio" id="samica" name="plec" value="samica" required>
+          <label for="samica">Samica</label><br><br>
+
+          Wykastrowany / Sterylizowany?<br>
+          <input type="radio" id="tak" name="wykastrowany"value="tak" required>
+          <label for="tak">Tak</label><br>
+          
+          <input type="radio" id="nie" name="wykastrowany" value="nie" required>
+          <label for="nie">Nie</label><br>
+
+          <br><br><input type="submit" class="btn btn-md btn-primary" value="Dodaj">
+          </div>
+        </form>
         <hr class="featurette-divider">
-        <a class="btn btn-md btn-primary" href="add_animal.php" role="button">Dodaj zwierzaka!</a>
+
       </div>
     </div>
   </main>
